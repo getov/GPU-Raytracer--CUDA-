b1dcd6078b7058d50b08413de1ff7643a2c03495
Peter Getov <peter.getov@gmail.com>
2014-01-13 09:58:09 -0800

    Anti-aliasing implemented. Oren-Nayar shader implemented.
    TODO: Still need to fix the screen bug (rendering the same scene multiple times)

--- CUDA RayTracer/CUDA RayTracer/cuda_renderer.cu
+++ CUDA RayTracer/CUDA RayTracer/cuda_renderer.cu
@@ -1,6 +1,6 @@
-#ifndef __CUDACC__  
-    #define __CUDACC__
-#endif
+//#ifndef __CUDACC__  
+//    #define __CUDACC__
+//#endif
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
@@ -15,6 +15,10 @@
 #include "IShader.cuh"
 #include "Lambert.cuh"
 #include "Sphere.cuh"
+#include "OrenNayar.cuh"
+
+__device__
+bool needsAA[RES_X][RES_Y];
 
 __device__ 
 bool testVisibility(Node** dev_nodes, const IntersectionData& data)
@@ -64,7 +68,7 @@ void initializeScene(Camera* dev_cam, Geometry** dev_geom, Shader** dev_shaders,
 	dev_cam->roll = 0;
 	dev_cam->fov = 90;
 	dev_cam->aspect = 4.0 / 3.0;
-	dev_cam->pos = Vector(0, 160, -50);
+	dev_cam->pos = Vector(0, 160, -10);
 
 	dev_cam->beginFrame();
 	
@@ -76,17 +80,26 @@ void initializeScene(Camera* dev_cam, Geometry** dev_geom, Shader** dev_shaders,
 	createNode(new Plane(5), new Lambert(Color(0, 1, 0)),
 			   dev_geom, dev_shaders, dev_nodes);
 
-	createNode(new Sphere(Vector(-80, 40, 180), 50.0), new Lambert(Color(1.0, 1.0, 0.0)),
+	createNode(new Sphere(Vector(-150, 40, 180), 20.0), new Lambert(Color(1.0, 1.0, 0.0)),
 			   dev_geom, dev_shaders, dev_nodes);
 
-	createNode(new Sphere(Vector(0, 30, 200), 50.0), new Lambert(Color(1.0, 0.0, 0.0)),
+	createNode(new Sphere(Vector(-50, 40, 180), 20.0), new Lambert(Color(0.5, 0.5, 0.5)),
 			   dev_geom, dev_shaders, dev_nodes);
 
-	/*createNode(new Sphere(Vector(0, 80, 200), 50.0), new Lambert(Color(0.0, 0.0, 1.0)),
+	createNode(new Sphere(Vector(-100, 40, 180), 20.0), new OrenNayar(Color(0.5, 0.5, 0.5), 1.0),
 			   dev_geom, dev_shaders, dev_nodes);
 
-	createNode(new Sphere(Vector(-80, 80, 200), 50.0), new Lambert(Color(0.0, 0.0, 1.0)),
-			   dev_geom, dev_shaders, dev_nodes);*/
+	createNode(new Sphere(Vector(0, 40, 180), 20.0), new OrenNayar(Color(0.5, 0.5, 0.5), 0.5),
+			   dev_geom, dev_shaders, dev_nodes);
+
+	createNode(new Sphere(Vector(-100, 40, 220), 20.0), new OrenNayar(Color(0.5, 0.5, 0.5), 0.5),
+			   dev_geom, dev_shaders, dev_nodes);
+
+	createNode(new Sphere(Vector(-50, 40, 220), 20.0), new OrenNayar(Color(0.0, 0.5, 0.5), 0.2),
+			   dev_geom, dev_shaders, dev_nodes);
+	
+	createNode(new Sphere(Vector(0, 40, 220), 20.0), new OrenNayar(Color(0.0, 0.0, 0.5), 0.9),
+			   dev_geom, dev_shaders, dev_nodes);
 
 
 }
@@ -117,6 +130,21 @@ Color raytrace(Ray ray, Geometry** dev_geom, Shader** dev_shaders, Node** dev_no
 	return closestNode->shader->shade(ray, data);
 }
 
+/**
+ * The function checks if one of the red, green or blue components
+ * of the colors a and b are too different.
+ * @return true - if the difference is bigger than the THRESHOLD
+ * @return false - if the difference is lower than the THRESHOLD
+*/
+__device__
+bool tooDifferent(const Color& a, const Color& b)
+{
+	const float THRESHOLD = 0.1;
+	return (fabs(a.r - b.r) > THRESHOLD ||
+		     fabs(a.g - b.g) > THRESHOLD ||
+		     fabs(a.b - b.b) > THRESHOLD);
+}
+
 __global__ 
 void renderScene(Color* dev_vfb, Camera* dev_cam, Geometry** dev_geom, Shader** dev_shaders, Node** dev_nodes)
 {
@@ -129,7 +157,52 @@ void renderScene(Color* dev_vfb, Camera* dev_cam, Geometry** dev_geom, Shader** 
 	
 	if (offset < RES_X * RES_Y)
 	{
+		// first pass
 		dev_vfb[offset] = raytrace(ray, dev_geom, dev_shaders, dev_nodes);
+
+		const double kernel[5][2] = {
+					{ 0, 0 },
+					{ 0.3, 0.3 },
+					{ 0.6, 0 },
+					{ 0, 0.6 },
+					{ 0.6, 0.6 },
+				};
+
+		// second pass
+		Color neighs[5];
+		neighs[0] = dev_vfb[offset];
+		neighs[1] = dev_vfb[(x > 0 ? x - 1 : x) + y * blockDim.x * gridDim.x];
+		neighs[2] = dev_vfb[(x + 1 < RES_X ? x + 1 : x) + y * blockDim.x * gridDim.x];
+		neighs[3] = dev_vfb[x + (y > 0 ? y - 1 : y) * blockDim.x * gridDim.x];
+		neighs[4] = dev_vfb[x + (y + 1 < RES_Y ? y + 1 : y) * blockDim.x * gridDim.x];
+
+		Color average(0, 0, 0);
+			
+		for (int i = 0; i < 5; i++)
+		{
+			average += neighs[i];
+		}
+		average /= 5.0f;
+			
+		for (int i = 0; i < 5; i++)
+		{
+			if (tooDifferent(neighs[i], average))
+			{
+				needsAA[x][y] = true;
+				break;
+			}
+		}
+
+		if (needsAA[x][y])
+		{
+			Color result = dev_vfb[offset]; 
+
+			for (int i = 1; i < 5; i++)
+			{
+				result += raytrace(dev_cam->getScreenRay(x + kernel[i][0], y + kernel[i][1]), dev_geom, dev_shaders, dev_nodes);
+			}
+			dev_vfb[offset] = result / 5.0f;
+		}
 	}
 }
 
