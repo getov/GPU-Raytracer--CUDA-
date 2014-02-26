getov/GPU-Raytracer--CUDA-

commit ccad5f3622afbe6f6bc5ed5d2393560c756b3b30

Real-Time Editing:  - Implemented Fog effect

CUDA RayTracer - OpenGL/CUDA RayTracer/EventHandler.cpp
@@ -288,7 +288,8 @@ void EventHandler::handleMouse()
 	glfwSetMousePos(0, 0);
 }
 
-bool EventHandler::keySwitch[4] = { false, false, false, false };
+bool EventHandler::keySwitch[10] = { false, false, false, false, false,
+									 false, false, false, false, false };
 
 void GLFWCALL EventHandler::keyboardCallback(int key, int action)
 {	
@@ -358,6 +359,18 @@ void GLFWCALL EventHandler::keyboardCallback(int key, int action)
 			else if (glfwGetKey(GLFW_KEY_LEFT) == GLFW_PRESS)
 			{
 				getPreviousShader();		
+			}
+
+			// Fog options
+			if (key == 'F' && !keySwitch[4])
+			{
+				useFog(true);
+				keySwitch[4] = true;
+			}
+			else if (key == 'F' && keySwitch[4])
+			{
+				useFog(false);
+				keySwitch[4] = false;
 			}
 		}
 		else

CUDA RayTracer - OpenGL/CUDA RayTracer/Node.cuh
@@ -29,21 +29,24 @@ public:
 	__device__
 	bool intersect(Ray ray, IntersectionData& data)
 	{
-		ray.start = transform.undoPoint(ray.start);
-		ray.dir = transform.undoDirection(ray.dir);
+		// transform from world space into model space
+		Ray modelRay;
+		modelRay.start = transform.undoPoint(ray.start);
+		modelRay.dir = transform.undoDirection(ray.dir);
+		modelRay.depth = ray.depth;
+
 		double oldDist = data.dist;
 		double rayDirLength = ray.dir.length();
 		data.dist *= rayDirLength;
-		ray.dir.normalize();
+		modelRay.dir.normalize();
 
-		bool res = geom->intersect(ray, data);
-
-		if (!res)
+		if (!geom->intersect(modelRay, data))
 		{
 			data.dist = oldDist;
 			return false;
 		}
 
+		// transform from model space into world space
 		data.normal = normalize(transform.direction(data.normal));
 		data.dNdx = normalize(transform.direction(data.dNdx));
 		data.dNdy = normalize(transform.direction(data.dNdy));

CUDA RayTracer - OpenGL/CUDA RayTracer/RaytracerControls.cu
@@ -313,3 +313,13 @@ extern "C" void generateSphere()
 {
 	gen_sphere<<<1, 1>>>();
 }
+
+// Fog options
+__global__ void use_fog(bool fog)
+{
+	scene->isFogActive = fog;
+}
+extern "C" void useFog(bool fog)
+{
+	use_fog<<<1, 1>>>(fog);
+}

CUDA RayTracer - OpenGL/CUDA RayTracer/RaytracerControls.cuh
@@ -96,4 +96,8 @@ extern "C" void getPreviousShader();
 __global__ void gen_sphere();
 extern "C" void generateSphere();
 
+// Fog options
+__global__ void use_fog(bool fog);
+extern "C" void useFog(bool fog);
+
 #endif

CUDA RayTracer - OpenGL/CUDA RayTracer/Scene.cu
@@ -4,6 +4,10 @@ __device__ Scene::Scene()
 	: selectedNode(nullptr)
 	, indexGeom(-1)
 	, indexShader(-1)
+	, isFogActive(false)
+	, fogDensity(150.0)
+	, expDecay(1.0)
+	, fogColor(0.5, 0.5, 0.5)
 {
 	ambientLight = Color(0.2, 0.2, 0.2);
 	waves = 0.2;

CUDA RayTracer - OpenGL/CUDA RayTracer/Scene.cuh
@@ -32,6 +32,12 @@ public:
 
 	double waves;
 	double timeInSecond;
+
+	// fog
+	bool isFogActive;
+	double fogDensity;
+	double expDecay;
+	Color fogColor;
 };
 
 #endif

CUDA RayTracer - OpenGL/CUDA RayTracer/cuda_renderer.cu
@@ -217,13 +217,27 @@ void update(double elapsedTime, double currentTime)
 {
 	scene->waves = currentTime;
 
-	scene->timeInSecond = (currentTime - elapsedTime) - static_cast<int>(currentTime - elapsedTime);
+	// record in which part of the second we are at [0.0; 0.9]
+	// and multiply it by PI, so we can get a value in the interval [0.0; PI]
+	scene->timeInSecond = ((currentTime - elapsedTime) - static_cast<int>(currentTime - elapsedTime)) * PI;
 }
 
 extern "C"
 void updateScene(const double& elapsedTime, const double& currentTime)
 {
 	update<<<1, 1>>>(elapsedTime, currentTime);
+}
+
+__device__
+Color BackgroundColor(const double& decay)
+{
+	return Color(0.55f, 0.8f, 0.95f) * decay;
+}
+
+__device__
+Color Fog(const double& decay)
+{
+	return Color(0.5, 0.5, 0.5) * (1.0 - decay);
 }
 
 __device__ 
@@ -246,7 +260,7 @@ Color raytrace(Ray ray)
 			closestNode = scene->dev_nodes[i];
 		}
 	}
-
+	
 	// check if the closest intersection point is actually a light:
 	bool hitLight = false;
 	Color hitLightColor;
@@ -260,11 +274,23 @@ Color raytrace(Ray ray)
 	}
 	if (hitLight) return hitLightColor;
 
+	double expDecay;
+	if (scene->isFogActive)
+	{
+		double minDist = data.dist;
+		//scene->expDecay = pow(0.5, minDist / scene->fogDensity);
+		
+		expDecay = pow(0.5, minDist / scene->fogDensity);
+	}
+	else
+	{
+		//scene->expDecay = 1.0;
+		expDecay = 1.0;
+	}
+
 	if (!closestNode)
 	{
-		//return Color(0, 0, 0);
-		return Color(0.55f, 0.8f, 0.95f); // skyblue
-		//return Color(1, 1, 1);
+		return BackgroundColor(expDecay) + Fog(expDecay); // skyblue
 	}
 
 	if (closestNode->bumpTex != nullptr)
@@ -274,17 +300,10 @@ Color raytrace(Ray ray)
 
 	if (closestNode == scene->selectedNode)
 	{
-		if (scene->timeInSecond < 0.5)
-		{
-			return closestNode->shader->shade(ray, data) * 3.5;
-		}
-		else
-		{
-			return closestNode->shader->shade(ray, data) * 0.5;
-		}		
+		return closestNode->shader->shade(ray, data) * sin(scene->timeInSecond) * 1.5;
 	}
-	
-	return closestNode->shader->shade(ray, data);
+
+	return closestNode->shader->shade(ray, data) * expDecay + Fog(expDecay);
 }
 
 /**
@@ -472,7 +491,7 @@ void cameraBeginFrame()
 extern "C" 
 void cudaRenderer(uchar4* dev_vfb)
 {
-	dim3 THREADS_PER_BLOCK(8, 8); // 8*8 - most optimal; 32*32 = 1024 (max threads per block supported)
+	dim3 THREADS_PER_BLOCK(4, 8); // 4*8 - most optimal; 32*32 = 1024 (max threads per block supported)
 	dim3 BLOCKS(GlobalSettings::RES_X / THREADS_PER_BLOCK.x, GlobalSettings::RES_Y / THREADS_PER_BLOCK.y); 
 
 	// first pass
@@ -483,7 +502,7 @@ void cudaRenderer(uchar4* dev_vfb)
 		//second pass
 		antiAliasing<<<BLOCKS, THREADS_PER_BLOCK>>>(dev_vfb, GlobalSettings::previewAA, GlobalSettings::RES_X, GlobalSettings::RES_Y);
 	}
-
+	
 	if (GlobalSettings::blur)
 	{
 		blurScene<<<BLOCKS, THREADS_PER_BLOCK>>>(dev_vfb, GlobalSettings::RES_X, GlobalSettings::RES_Y);

commit a4981d56230d3e551a8c291cf79b50b51d16d85c

Fixed Bugs:  - There was a bug in Node class that caused incorrect object transformations    since revision: ccad5f3622afbe6f6bc5ed5d2393560c756b3b30 (the Fog commit)
Real-Time Editing
 - Now the Fog level can be changed via the mouse wheel
 - added feature to the CameraController class that inclines the camera around the    'Z' axis in the range -15 to 15 degrees

CUDA RayTracer - OpenGL/CUDA RayTracer/Camera.cu
@@ -22,6 +22,26 @@
 
 #include "Camera.cuh"
 
+__device__
+void Camera::applyOrientation()
+{
+	Matrix rotation = rotationAroundZ(toRadians(roll))  *
+			   rotationAroundX(toRadians(pitch)) *
+	           rotationAroundY(toRadians(yaw));
+
+	upLeft   *= rotation;
+	upRight  *= rotation;
+	downLeft *= rotation;
+
+	rightDir = Vector(1, 0, 0) * rotation;
+	upDir    = Vector(0, 1, 0) * rotation;
+	frontDir = Vector(0, 0, 1) * rotation;
+	
+	upLeft   += pos;
+	upRight  += pos;
+	downLeft += pos;
+}
+
 __device__ 
 void Camera::beginFrame()
 {
@@ -39,23 +59,25 @@ void Camera::beginFrame()
 	x *= scaling;
 	y *= scaling;
 
-	this->upLeft = Vector(x, -y, 1);
-	this->upRight = Vector(-x, -y, 1);
-	this->downLeft = Vector(x, y, 1);
+	this->upLeft   = Vector(x, -y, 1);
+	this->upRight  = Vector(-x, -y, 1);
+	this->downLeft = Vector(x,  y, 1);
 	
+	//applyOrientation();
 	Matrix rotation = rotationAroundZ(toRadians(roll))
 	                * rotationAroundX(toRadians(pitch))
 	                * rotationAroundY(toRadians(yaw));
-	upLeft *= rotation;
-	upRight *= rotation;
+
+	upLeft   *= rotation;
+	upRight  *= rotation;
 	downLeft *= rotation;
 
 	rightDir = Vector(1, 0, 0) * rotation;
 	upDir    = Vector(0, 1, 0) * rotation;
 	frontDir = Vector(0, 0, 1) * rotation;
 	
-	upLeft += pos;
-	upRight += pos;
+	upLeft   += pos;
+	upRight  += pos;
 	downLeft += pos;
 }
 

CUDA RayTracer - OpenGL/CUDA RayTracer/Camera.cuh
@@ -29,7 +29,12 @@
 
 class Camera 
 {
+private:
 	Vector upLeft, upRight, downLeft;
+	//Matrix rotation;
+
+	__device__
+	void applyOrientation();
 	
 public:
 	Vector frontDir, rightDir, upDir;

CUDA RayTracer - OpenGL/CUDA RayTracer/CameraController.cu
@@ -4,6 +4,8 @@ __device__
 CameraController::CameraController(Camera& camera, const float& speed)
 	: m_camera(&camera)
 	, movementSpeed(speed)
+	, q(0.0)
+	, n(1.0)
 {
 }
 
@@ -34,25 +36,25 @@ Vector CameraController::left()
 __device__ 
 void CameraController::moveForward()
 {
-	m_camera->pos += forward() * movementSpeed;
+	m_camera->pos += forward() * movementSpeed * scene->secondsElapsed;
 }
 
 __device__
 void CameraController::moveBackward()
 {
-	m_camera->pos += backward() * movementSpeed;
+	m_camera->pos += backward() * movementSpeed * scene->secondsElapsed;
 }
 
 __device__ 
 void CameraController::strafeRight()
 {
-	m_camera->pos += right() * movementSpeed;
+	m_camera->pos += right() * movementSpeed * scene->secondsElapsed;
 }
 
 __device__ 
 void CameraController::strafeLeft()
 {
-	m_camera->pos += left() * movementSpeed;
+	m_camera->pos += left() * movementSpeed * scene->secondsElapsed;
 }
 
 // rotation
@@ -80,4 +82,23 @@ void CameraController::offsetCameraOrientation(const float& zenith, const float&
 	{
 		m_camera->pitch = -85.0f;
 	}
+}
+
+__device__
+void CameraController::quaternion()
+{
+	q += n * scene->secondsElapsed;
+
+	if (q > PI/12)
+	{
+		q = PI/12;
+		n *= -1.0;
+	}
+	else if (q < -PI/12)
+	{
+		q = -PI/12;
+		n *= -1.0;
+	}
+
+	m_camera->roll = toDegrees(asin(sin(q)));
 }

CUDA RayTracer - OpenGL/CUDA RayTracer/CameraController.cuh
@@ -2,12 +2,16 @@
 #define CAMERA_CONTROLLER_H
 
 #include "Camera.cuh"
+#include "Scene.cuh"
 
 class CameraController
 {
 private:
 	Camera* m_camera;
 	float movementSpeed;
+
+	double q;
+	double n;
 
 	/**
 	 * @brief - Wrapper function for returning
@@ -80,6 +84,8 @@ public:
 	*/
 	__device__
 	void offsetCameraOrientation(const float& zenith, const float& azimuth);
+
+	__device__ void quaternion();
 };
 
 #endif 

CUDA RayTracer - OpenGL/CUDA RayTracer/EventHandler.cpp
@@ -24,6 +24,7 @@ void EventHandler::handleKeyboard()
 	if (glfwGetKey('W'))
 	{
 		moveForward();
+		//interpolateQuat();
 	}
 
 	if (glfwGetKey('S'))
@@ -286,6 +287,13 @@ void EventHandler::handleMouse()
 
 	glfwDisable(GLFW_MOUSE_CURSOR);
 	glfwSetMousePos(0, 0);
+
+	if (GlobalSettings::isEditingAllowed && keySwitch[4])
+	{
+		fogLevel((float)glfwGetMouseWheel() * (-25.f));
+	}
+
+	glfwSetMouseWheel(0);
 }
 
 bool EventHandler::keySwitch[10] = { false, false, false, false, false,
@@ -371,7 +379,7 @@ void GLFWCALL EventHandler::keyboardCallback(int key, int action)
 			{
 				useFog(false);
 				keySwitch[4] = false;
-			}
+			}	
 		}
 		else
 		{

CUDA RayTracer - OpenGL/CUDA RayTracer/Node.cuh
@@ -36,7 +36,7 @@ public:
 		modelRay.depth = ray.depth;
 
 		double oldDist = data.dist;
-		double rayDirLength = ray.dir.length();
+		double rayDirLength = modelRay.dir.length();
 		data.dist *= rayDirLength;
 		modelRay.dir.normalize();
 

CUDA RayTracer - OpenGL/CUDA RayTracer/RaytracerControls.cu
@@ -57,6 +57,15 @@ void setCameraOrientation(float zenith, float azimuth)
 	setCamOrientation<<<1, 1>>>(zenith, azimuth);
 }
 
+__global__ void quat()
+{
+	controller->quaternion();
+}
+extern "C" void interpolateQuat()
+{
+	quat<<<1, 1>>>();
+}
+
 // Target Geometries
 __global__ void target_next_geom()
 {
@@ -322,4 +331,23 @@ __global__ void use_fog(bool fog)
 extern "C" void useFog(bool fog)
 {
 	use_fog<<<1, 1>>>(fog);
+}
+
+__global__ void fog_level(float fogMult)
+{
+	scene->fogDensity += fogMult;
+
+	if (scene->fogDensity <= 0)
+	{
+		scene->fogDensity = 1.0;
+	}
+
+	if (scene->fogDensity > 1600.0)
+	{
+		scene->fogDensity = 1600.0;
+	}
+}
+extern "C" void fogLevel(float fogMult)
+{
+	fog_level<<<1, 1>>>(fogMult);
 }

CUDA RayTracer - OpenGL/CUDA RayTracer/RaytracerControls.cuh
@@ -28,6 +28,9 @@ extern "C" void strafeRight();
 __global__ void setCamOrientation(float zenith, float azimuth);
 extern "C" void setCameraOrientation(float zenith, float azimuth);
 
+__global__ void quat();
+extern "C" void interpolateQuat();
+
 // Object transformations
 __global__ void target_next_geom();
 extern "C" void targetNextGeometry();
@@ -99,5 +102,8 @@ extern "C" void generateSphere();
 // Fog options
 __global__ void use_fog(bool fog);
 extern "C" void useFog(bool fog);
+
+__global__ void fog_level(float fogMult);
+extern "C" void fogLevel(float fogMult);
 
 #endif

CUDA RayTracer - OpenGL/CUDA RayTracer/Scene.cu
@@ -6,7 +6,6 @@ __device__ Scene::Scene()
 	, indexShader(-1)
 	, isFogActive(false)
 	, fogDensity(150.0)
-	, expDecay(1.0)
 	, fogColor(0.5, 0.5, 0.5)
 {
 	ambientLight = Color(0.2, 0.2, 0.2);

CUDA RayTracer - OpenGL/CUDA RayTracer/Scene.cuh
@@ -30,13 +30,14 @@ public:
 	int indexGeom;
 	int indexShader;
 
+	// time
 	double waves;
 	double timeInSecond;
+	double secondsElapsed;
 
 	// fog
 	bool isFogActive;
 	double fogDensity;
-	double expDecay;
 	Color fogColor;
 };
 

CUDA RayTracer - OpenGL/CUDA RayTracer/cuda_renderer.cu
@@ -89,7 +89,7 @@ void initializeScene(short sceneID, int RES_X, int RES_Y, bool realTime)
 	scene->dev_cam->aspect = static_cast<float>(RES_X) / RES_Y;
 	scene->dev_cam->pos = Vector(0, 150, -100);
 
-	controller = new CameraController(*(scene->dev_cam), 10.f);
+	controller = new CameraController(*(scene->dev_cam), 100.f);
 
 	if (realTime)
 	{
@@ -215,6 +215,8 @@ void initializeScene(short sceneID, int RES_X, int RES_Y, bool realTime)
 __global__
 void update(double elapsedTime, double currentTime)
 {
+	scene->secondsElapsed = elapsedTime;
+
 	scene->waves = currentTime;
 
 	// record in which part of the second we are at [0.0; 0.9]
@@ -237,7 +239,7 @@ Color BackgroundColor(const double& decay)
 __device__
 Color Fog(const double& decay)
 {
-	return Color(0.5, 0.5, 0.5) * (1.0 - decay);
+	return scene->fogColor * (1.0 - decay);
 }
 
 __device__ 